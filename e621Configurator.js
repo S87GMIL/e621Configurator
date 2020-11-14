f79d87ff625fa1a51855870ee15826c70a09fb73
S87GMIL <S.Miller87@gmx.net>
2020-11-15 00:14:43 +0100

    Splitted the configurator logic and the main tampermonkey script

--- e621Configurator.js
+++ e621Configurator.js
@@ -1,27 +1,7 @@
-// ==UserScript==
-// @name         e621 configurator
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Configure e621 to your liking
-// @author       S87GMIL
-// @match        https://e621.net/*
-// @icon64       https://raw.githubusercontent.com/S87GMIL/e621Configurator/master/icons/test.png
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/master/classes/HtmlFunctions.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/master/classes/PostsViewParser.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/master/classes/PostViewConfiguration.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/master/classes/Profile.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/master/classes/SetsViewConfiguration.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/master/classes/SetsViewParser.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/master/classes/ViewConfigParser.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/master/classes/ViewConfigurator.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/master/utils/ProfileStorage.js
-// @grant        GM_setValue
-// @grant        GM_getValue
-// @grant        GM_deleteValue
-// ==/UserScript==
-
 (function () {
     'use strict';
+
+    var oHtmlFunctions = new HtmlFunctions();
 
     function createNewProfile(sId, sName, sDescription, oIsActive, oViewConfigs, bDeletable, bEditable) {
         return new Profile(sId, sName, sDescription, oIsActive, oViewConfigs, bDeletable, bEditable);
@@ -2117,7 +2097,7 @@
         });
     }
 
-    function initialize() {
+    function initializeE621Configurator() {
         var oActiveProfile;
         var oCreatedConfigs = ProfileStorage.loadCreatedProfiles();
 
@@ -2142,6 +2122,4 @@
         }
     }
 
-    var oHtmlFunctions = new HtmlFunctions();
-    initialize();
-})();
+});
