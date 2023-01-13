107a8ba8507372928760178900424e83075fc62d
S87GMIL <S.Miller87@gmx.net>
2023-01-13 18:50:55 +0100

    Iterated the version number

--- e621Configurator.user.js
+++ e621Configurator.user.js
@@ -1,28 +1,28 @@
 // ==UserScript==
 // @name         e621 configurator
 // @namespace    http://tampermonkey.net/
-// @version      2.3
+// @version      2.4
 // @description  Configure e621 to your liking
 // @author       S87GMIL
 // @match        https://e621.net/*
-// @icon64       https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.3/icons/e621Configurator.png
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.3/utils/HTMLFunctions.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.3/utils/ProfileStorage.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.3/utils/URLFunctions.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.3/utils/DataBuffer.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.3/helpers/UIHelper.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.3/classes/ConfigurationSection.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.3/classes/ElementSelection.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.3/classes/ViewConfiguration.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.3/classes/ViewConfigParser.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.3/classes/PostsViewParser.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.3/classes/PostViewConfiguration.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.3/classes/Profile.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.3/classes/SetsViewConfiguration.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.3/classes/SetsViewParser.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.3/helpers/APIHelper.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.3/helpers/SuggestionHelper.js
-// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.3/classes/e621Configurator.js
+// @icon64       https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.4/icons/e621Configurator.png
+// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.4/utils/HTMLFunctions.js
+// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.4/utils/ProfileStorage.js
+// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.4/utils/URLFunctions.js
+// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.4/utils/DataBuffer.js
+// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.4/helpers/UIHelper.js
+// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.4/classes/ConfigurationSection.js
+// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.4/classes/ElementSelection.js
+// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.4/classes/ViewConfiguration.js
+// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.4/classes/ViewConfigParser.js
+// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.4/classes/PostsViewParser.js
+// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.4/classes/PostViewConfiguration.js
+// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.4/classes/Profile.js
+// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.4/classes/SetsViewConfiguration.js
+// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.4/classes/SetsViewParser.js
+// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.4/helpers/APIHelper.js
+// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.4/helpers/SuggestionHelper.js
+// @require      https://raw.githubusercontent.com/S87GMIL/e621Configurator/v2.4/classes/e621Configurator.js
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_deleteValue
